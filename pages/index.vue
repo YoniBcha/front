039dc74f5469098fb69bba23604327300b23d28c
Yo <yonansmillion29@example.com>
2024-04-16 02:02:47 +0300

    sidebar is in progress

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,72 @@
 <template>
-  <div class="text-red-600">Root Page</div>
+  <a-layout class="">
+    <a-layout-sider
+      v-model:collapsed="collapsed"
+      :trigger="null"
+      collapsible
+      class="h-[100vh]"
+    >
+      <div class="h-[50px]">logo</div>
+      <a-menu v-model:selectedKeys="selectedKeys" theme="dark" mode="inline">
+        <a-menu-item key="1">
+          <user-outlined />
+          <span>nav 1</span>
+        </a-menu-item>
+        <a-menu-item key="2">
+          <video-camera-outlined />
+          <span>nav 2</span>
+        </a-menu-item>
+        <a-menu-item key="3">
+          <upload-outlined />
+          <span>nav 3</span>
+        </a-menu-item>
+      </a-menu>
+    </a-layout-sider>
+    <a-layout>
+      <a-layout-header style="background: #fff; padding: 0">
+        <menu-unfold-outlined
+          v-if="collapsed"
+          class="text-3xl ml-5"
+          @click="() => (collapsed = !collapsed)"
+        />
+        <menu-fold-outlined
+          v-else
+          class="text-3xl ml-5"
+          @click="() => (collapsed = !collapsed)"
+        />
+      </a-layout-header>
+      <a-layout-content
+        :style="{
+          margin: '24px 16px',
+          padding: '24px',
+          background: '#fff',
+          minHeight: '280px',
+        }"
+      >
+        Content
+      </a-layout-content>
+    </a-layout>
+  </a-layout>
 </template>
 
-<script>
-export default {};
+<script setup>
+import { ref } from "vue";
+const selectedKeys = ref(["1"]);
+const collapsed = ref(false);
 </script>
+
+<style>
+#components-layout-demo-custom-trigger .trigger:hover {
+  color: #1890ff;
+}
+
+#components-layout-demo-custom-trigger .logo {
+  height: 32px;
+  background: rgba(255, 255, 255, 0.3);
+  margin: 16px;
+}
+
+.site-layout .site-layout-background {
+  background: #fff;
+}
+</style>
