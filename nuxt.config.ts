ebb5003aeaebf6d245ebc69da47b23bdb896b5d7
Yo <yonansmillion29@example.com>
2024-04-16 01:23:33 +0300

    ant design module is added

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  antd: {},
+  css: ["~/assets/css/main.css"],
   devtools: { enabled: true },
-  css: ['~/assets/css/main.css'],
+  modules: ["@ant-design-vue/nuxt"],
   postcss: {
     plugins: {
       tailwindcss: {},
       autoprefixer: {},
     },
   },
-  ssr: false
-})
+  ssr: false,
+});
