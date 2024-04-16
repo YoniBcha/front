863d1d70a65215ffc2b7147ec3a72ab556f7fc07
Yo <yonansmillion29@example.com>
2024-04-17 02:54:38 +0300

    jobless page is in progress

--- pages/jobless_registration.vue
+++ pages/jobless_registration.vue
@@ -1,7 +1,26 @@
 <template>
-  <div>Jobless Registration</div>
+  <div class="">
+    <div class="flex justify-between">
+      <div class="font-bold text-lg text-[#0a58a4]">Jobless Registration</div>
+      <div class="">
+        <button
+          class="bg-[#0a58a4] text-white px-5 py-2 rounded-lg text-semibold"
+        >
+          New Jobless
+        </button>
+      </div>
+    </div>
+    <div class="mt-5">
+      <a-input-search
+        placeholder="search jobless person"
+        @search="onSearch"
+        :style="{ width: '300px'}"
+      />
+    </div>
+  </div>
 </template>
 
-<script>
-export default {};
+<script setup>
+
+
 </script>
