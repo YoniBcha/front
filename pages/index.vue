872fda3831e128c03834148ee2724a8612b202d5
Yo <yonansmillion29@example.com>
2024-04-16 03:55:51 +0300

    sidebar is in progress

--- pages/index.vue
+++ pages/index.vue
@@ -1,24 +1,54 @@
 <template>
-  <a-layout class="">
+  <a-layout :style="{ width: layoutWidth }">
     <a-layout-sider
       v-model:collapsed="collapsed"
       :trigger="null"
       collapsible
-      class="h-[100vh]"
+      :width="siderWidth"
+      class="h-[100vh] bg-red-600"
+      :style="sidebarStyle"
     >
-      <div class="h-[50px]">logo</div>
-      <a-menu v-model:selectedKeys="selectedKeys" theme="dark" mode="inline">
-        <a-menu-item key="1">
-          <user-outlined />
-          <span>nav 1</span>
+      <div class="flex justify-center items-center h-[60px] text-white">
+        LOGO
+      </div>
+      <a-menu
+        v-model:selectedKeys="selectedKeys"
+        theme="dark"
+        mode="inline"
+        class="mt-5"
+      >
+        <a-menu-item key="1" class="text-xl">
+          <pie-chart-outlined />
+          <span>Dashboard</span>
         </a-menu-item>
         <a-menu-item key="2">
-          <video-camera-outlined />
-          <span>nav 2</span>
+          <desktop-outlined />
+          <span>Option 2</span>
         </a-menu-item>
-        <a-menu-item key="3">
-          <upload-outlined />
-          <span>nav 3</span>
+        <a-sub-menu key="sub1">
+          <template #title>
+            <span>
+              <user-outlined />
+              <span>User</span>
+            </span>
+          </template>
+          <a-menu-item key="3">Tom</a-menu-item>
+          <a-menu-item key="4">Bill</a-menu-item>
+          <a-menu-item key="5">Alex</a-menu-item>
+        </a-sub-menu>
+        <a-sub-menu key="sub2">
+          <template #title>
+            <span>
+              <team-outlined />
+              <span>Team</span>
+            </span>
+          </template>
+          <a-menu-item key="6">Team 1</a-menu-item>
+          <a-menu-item key="8">Team 2</a-menu-item>
+        </a-sub-menu>
+        <a-menu-item key="9">
+          <file-outlined />
+          <span>File</span>
         </a-menu-item>
       </a-menu>
     </a-layout-sider>
@@ -26,47 +56,35 @@
       <a-layout-header style="background: #fff; padding: 0">
         <menu-unfold-outlined
           v-if="collapsed"
-          class="text-3xl ml-5"
+          class="text-3xl ml-3"
           @click="() => (collapsed = !collapsed)"
         />
         <menu-fold-outlined
           v-else
-          class="text-3xl ml-5"
+          class="text-3xl ml-3"
           @click="() => (collapsed = !collapsed)"
         />
       </a-layout-header>
-      <a-layout-content
-        :style="{
-          margin: '24px 16px',
-          padding: '24px',
-          background: '#fff',
-          minHeight: '280px',
-        }"
-      >
-        Content
-      </a-layout-content>
+      <a-layout-content :style="contentStyle"> Content </a-layout-content>
     </a-layout>
   </a-layout>
 </template>
 
 <script setup>
-import { ref } from "vue";
+import { ref, reactive } from "vue";
+
 const selectedKeys = ref(["1"]);
 const collapsed = ref(false);
+const layoutWidth = "100%";
+const siderWidth = "260px";
+
+const contentStyle = reactive({
+  margin: "24px 16px",
+  padding: "24px",
+  background: "#fff",
+  minHeight: "280px",
+});
+const sidebarStyle = reactive({
+  background: "#1518ba"
+});
 </script>
-
-<style>
-#components-layout-demo-custom-trigger .trigger:hover {
-  color: #1890ff;
-}
-
-#components-layout-demo-custom-trigger .logo {
-  height: 32px;
-  background: rgba(255, 255, 255, 0.3);
-  margin: 16px;
-}
-
-.site-layout .site-layout-background {
-  background: #fff;
-}
-</style>
