631ef805be3b2cadacd2604bf55abd47a0fd55ab
Yo <yonansmillion29@example.com>
2024-04-16 07:44:48 +0300

    profile button and image is made

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,42 @@
+<script setup>
+import { ref, reactive } from "vue";
+
+const selectedKeys = ref(["1"]);
+const collapsed = ref(false);
+const layoutWidth = "100%";
+const siderWidth = "260px";
+
+const contentStyle = reactive({
+  margin: "24px 16px",
+  padding: "24px",
+  background: "#fff",
+  minHeight: "280px",
+});
+const sidebarStyle = reactive({
+  background: "#1518ba",
+});
+const sidebarMenuStyle = reactive({
+  background: "#1518ba",
+});
+const sidebarMenuItemStyle = reactive({
+  fontSize: "20px",
+  display: "flex",
+  alignItems: "center",
+  height: "50px",
+});
+
+const submenuStyle = reactive({
+  fontSize: "20px",
+  background: "#1518ba",
+});
+
+const showDropdown = ref(false);
+
+function toggleDropdown() {
+  showDropdown.value = !showDropdown.value;
+}
+</script>
+
 <template>
   <a-layout :style="{ width: layoutWidth }">
     <a-layout-sider
@@ -70,17 +109,36 @@
             @click="() => (collapsed = !collapsed)"
           />
         </div>
-        <div class="flex gap-3 items-center h-full pr-5">
+        <div class="flex relative gap-3 items-center h-full pr-5">
           <div class="flex flex-col justify-center h-full">
             <div class="text-xl">Yonas</div>
-            <div class="text-sm text-gray-400">software developer</div>
+            <div class="text-sm text-gray-400">Admin, enterprise manager</div>
           </div>
           <div class="">
             <img
+              @click="toggleDropdown"
               class="object-cover w-12 h-12 rounded-full ring ring-gray-300 dark:ring-gray-600"
               src="https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=4&w=880&h=880&q=100"
               alt=""
             />
+            <ul
+              v-if="showDropdown"
+              class="absolute p-4 w-56 right-8 bg-white border-2 border-gray-200 rounded-md shadow-xl mt-3"
+            >
+              <li class="flex justify-between text-sm">
+                <div class="">Profile</div>
+                <div class="text-black pr-3">
+                  <LogoutOutlined />
+                </div>
+              </li>
+              <a-divider />
+              <li class="flex justify-between text-sm mt-2">
+                <div class="">Setting</div>
+                <div class="text-black pr-3">
+                  <SettingOutlined />
+                </div>
+              </li>
+            </ul>
           </div>
         </div>
       </a-layout-header>
@@ -88,36 +146,3 @@
     </a-layout>
   </a-layout>
 </template>
-
-<script setup>
-import { ref, reactive } from "vue";
-
-const selectedKeys = ref(["1"]);
-const collapsed = ref(false);
-const layoutWidth = "100%";
-const siderWidth = "260px";
-
-const contentStyle = reactive({
-  margin: "24px 16px",
-  padding: "24px",
-  background: "#fff",
-  minHeight: "280px",
-});
-const sidebarStyle = reactive({
-  background: "#1518ba",
-});
-const sidebarMenuStyle = reactive({
-  background: "#1518ba",
-});
-const sidebarMenuItemStyle = reactive({
-  fontSize: "20px",
-  display: "flex",
-  alignItems: "center",
-  height: "50px",
-});
-
-const submenuStyle = reactive({
-  fontSize: "20px",
-  background: "#1518ba",
-});
-</script>
