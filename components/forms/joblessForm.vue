4b5502231c8c44548287c714f3b66e14ffa44ec2
Yo <yonansmillion29@example.com>
2024-04-19 13:02:40 +0300

    jobless form

--- components/forms/joblessForm.vue
+++ components/forms/joblessForm.vue
@@ -7,8 +7,8 @@
     @finish="onFinish"
   >
     <div class="">
+      <!-- upload profile photo -->
       <div class="grid grid-cols-3 gap-4 h-48 m-5">
-        <!-- photo upload -->
         <div class="">
           <a-form-item style="width: 100%">
             <a-form-item name="dragger" no-style>
@@ -95,14 +95,96 @@
           />
         </div>
       </div>
+
       <div class="mx-5 mt-12">
         <a-input
-            v-model:value="jobless_grandfather_name"
-            placeholder="please enter your profession to tran and work with"
+          v-model:value="jobless_grandfather_name"
+          placeholder="please enter your profession to tran and work with"
+          allow-clear
+          class="mb-6"
+        />
+      </div>
+
+      <!-- upload identification card -->
+      <div class="grid grid-cols-3 gap-4 h-48 m-3">
+        <div class="flex flex-col p-2">
+          <a-input
+            v-model:value="jobless_housenumber"
+            placeholder="enter your house number"
             allow-clear
             class="mb-6"
           />
+          <a-input
+            type="number"
+            v-model:value="jobless_familysize"
+            placeholder="enter your fmaily size"
+            allow-clear
+            class="mb-6"
+          />
+          <a-input
+            v-model:value="jobless_livingstatus"
+            placeholder="enter your living status"
+            allow-clear
+          />
+        </div>
+        <div class="pt-2 left-0">
+          <a-input
+            v-model:value="jobless_birthplace"
+            placeholder="please enter your birth place"
+            allow-clear
+            class="mb-6"
+          />
+          <a-input
+            v-model:value="jobless_family_status"
+            placeholder="enter your family status"
+            allow-clear
+            class="mb-6"
+          />
+          <a-input
+            v-model:value="jobless_martial_status"
+            placeholder="enter your martial status"
+            allow-clear
+          />
+        </div>
+        <a-form-item class="pl-8 pb-18 w-96">
+          <a-form-item name="dragger" no-style>
+            <a-upload-dragger
+              v-model:fileList="formState.dragger"
+              name="files"
+              action="/upload.do"
+            >
+              <p class="ant-upload-drag-icon">
+                <InboxOutlined />
+              </p>
+              <p class="ant-upload-text">
+                Click or drag your photo to this area to upload
+              </p>
+              <p class="ant-upload-hint">
+                Support for a single or bulk upload.
+              </p>
+            </a-upload-dragger>
+          </a-form-item>
+        </a-form-item>
       </div>
+
+      <div class="flex mx-5 mt-5">
+        <a-input
+          v-model:value="jobless_grandfather_name"
+          placeholder="please enter disability staus"
+          allow-clear
+        />
+        <a-input
+          v-model:value="jobless_grandfather_name"
+          placeholder="enter your reason to come"
+          allow-clear
+          class="ml-5"
+        />
+      </div>
+
+      <div class="grid grid-cols-3 gap-4 h-48 m-5">
+        
+      </div>
+
     </div>
   </a-form>
 </template>
@@ -121,10 +203,4 @@ const formState = reactive({
   "checkbox-group": ["A", "B"],
   rate: 3.5,
 });
-const onFinish = (values) => {
-  console.log("Success:", values);
-};
-const onFinishFailed = (errorInfo) => {
-  console.log("Failed:", errorInfo);
-};
 </script>
