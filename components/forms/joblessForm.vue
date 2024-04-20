1d397819161d9d495b629e0d069c77fc60b25f52
Yo <yonansmillion29@example.com>
2024-04-20 16:20:46 +0300

    jobless form ui is done

--- components/forms/joblessForm.vue
+++ components/forms/joblessForm.vue
@@ -13,16 +13,17 @@
           <a-form-item style="width: 100%">
             <a-form-item name="dragger" no-style>
               <a-upload-dragger
-                v-model:fileList="formState.dragger"
-                name="files"
+                v-model:fileList="formState.photo"
+                name="photo"
                 action="/upload.do"
+                :rules="[
+                  { required: false, message: 'Please upload your photo!' },
+                ]"
               >
                 <p class="ant-upload-drag-icon">
                   <InboxOutlined />
                 </p>
-                <p class="ant-upload-text">
-                  Click or drag your photo to this area to upload
-                </p>
+                <p class="ant-upload-text">Click or drag your photo here</p>
                 <p class="ant-upload-hint">
                   Support for a single or bulk upload.
                 </p>
@@ -33,26 +34,34 @@
         <div class="flex flex-col">
           <a-input
             v-model:value="jobless_fullname"
+            :rules="[
+              { required: false, message: 'Please input your fullname!' },
+            ]"
             placeholder="enter your full name"
             allow-clear
             class="mb-6"
           />
           <a-select
-            v-model:value="formState.region"
+            v-model:value="formState.sex"
+            :rules="[{ required: false, message: 'Please select your sex!' }]"
             placeholder="please enter your gender"
             class="mb-6"
           >
-            <a-select-option value="shanghai">Male</a-select-option>
-            <a-select-option value="beijing">Female</a-select-option>
+            <a-select-option value="male">Male</a-select-option>
+            <a-select-option value="female">Female</a-select-option>
           </a-select>
           <a-input
             v-model:value="jobless_city"
+            :rules="[{ required: false, message: 'Please select your sex!' }]"
             placeholder="enter your city name"
             allow-clear
             class="mb-6"
           />
           <a-input
             v-model:value="jobless_phonenumber"
+            :rules="[
+              { required: false, message: 'Please select your phone number!' },
+            ]"
             placeholder="enter your phone number"
             allow-clear
             class="mb-6"
@@ -61,6 +70,12 @@
         <div class="">
           <a-input
             v-model:value="jobless_grandfather_name"
+            :rules="[
+              {
+                required: false,
+                message: 'Please select your grandfather name!',
+              },
+            ]"
             placeholder="please enter your grandfather name"
             allow-clear
             class="mb-6"
@@ -69,12 +84,16 @@
             <a-input
               type="number"
               v-model:value="jobless_age"
+              :rules="[{ required: false, message: 'Please select your age!' }]"
               placeholder="your age"
               allow-clear
               class="mb-6"
             />
             <a-input
               v-model:value="jobless_kebele"
+              :rules="[
+                { required: false, message: 'Please select your kebele!' },
+              ]"
               placeholder="your kebele"
               allow-clear
               class="mb-6 ml-2"
@@ -98,7 +117,13 @@
 
       <div class="mx-5 mt-12">
         <a-input
-          v-model:value="jobless_grandfather_name"
+          v-model:value="jobless_profession"
+          :rules="[
+            {
+              required: false,
+              message: 'Please select your profession to tran please!',
+            },
+          ]"
           placeholder="please enter your profession to tran and work with"
           allow-clear
           class="mb-6"
@@ -110,6 +135,9 @@
         <div class="flex flex-col p-2">
           <a-input
             v-model:value="jobless_housenumber"
+            :rules="[
+              { required: false, message: 'Please select your house number!' },
+            ]"
             placeholder="enter your house number"
             allow-clear
             class="mb-6"
@@ -117,12 +145,18 @@
           <a-input
             type="number"
             v-model:value="jobless_familysize"
+            :rules="[
+              { required: false, message: 'Please select your family size!' },
+            ]"
             placeholder="enter your fmaily size"
             allow-clear
             class="mb-6"
           />
           <a-input
             v-model:value="jobless_livingstatus"
+            :rules="[
+              { required: false, message: 'Please select your living status!' },
+            ]"
             placeholder="enter your living status"
             allow-clear
           />
@@ -130,18 +164,30 @@
         <div class="pt-2 left-0">
           <a-input
             v-model:value="jobless_birthplace"
+            :rules="[
+              { required: false, message: 'Please select your birth place!' },
+            ]"
             placeholder="please enter your birth place"
             allow-clear
             class="mb-6"
           />
           <a-input
             v-model:value="jobless_family_status"
+            :rules="[
+              { required: false, message: 'Please select your fmily status!' },
+            ]"
             placeholder="enter your family status"
             allow-clear
             class="mb-6"
           />
           <a-input
             v-model:value="jobless_martial_status"
+            :rules="[
+              {
+                required: false,
+                message: 'Please select your martial status!',
+              },
+            ]"
             placeholder="enter your martial status"
             allow-clear
           />
@@ -181,13 +227,100 @@
         />
       </div>
 
-      <div class="grid grid-cols-3 gap-4 h-48 m-5">
-        
+      <!-- profession certificate and jobless evidence card -->
+      <div class="grid grid-cols-2 gap-4 h-48 m-5 pt-2 mb-20">
+        <div class="">
+          <label for="" class="text-lg text-gray-600"
+            >If you and any certificate in specific profection</label
+          >
+          <a-form-item class="mt-4">
+            <a-form-item name="dragger" no-style>
+              <a-upload-dragger
+                v-model:fileList="formState.dragger"
+                name="files"
+                action="/upload.do"
+              >
+                <p class="ant-upload-drag-icon">
+                  <InboxOutlined />
+                </p>
+                <p class="ant-upload-text">
+                  Click or drag your profession cirtificate
+                </p>
+                <p class="ant-upload-hint">
+                  Support for a single or bulk upload.
+                </p>
+              </a-upload-dragger>
+            </a-form-item>
+          </a-form-item>
+        </div>
+        <div class="">
+          <div class="">
+            <a-input
+              v-model:value="jobless_grandfather_name"
+              placeholder="please enter profession name"
+              allow-clear
+            />
+          </div>
+          <div class="mt-5">
+            <label for="" class="text-lg text-gray-600"
+              >Upload Jobless evidence card here</label
+            >
+            <a-form-item class="mt-4">
+              <a-form-item name="dragger" no-style>
+                <a-upload-dragger
+                  v-model:fileList="formState.dragger"
+                  name="files"
+                  action="/upload.do"
+                >
+                  <p class="ant-upload-drag-icon">
+                    <InboxOutlined />
+                  </p>
+                  <p class="ant-upload-text">
+                    Click or drag your jobless evidence
+                  </p>
+                  <p class="ant-upload-hint">
+                    Support for a single or bulk upload.
+                  </p>
+                </a-upload-dragger>
+              </a-form-item>
+            </a-form-item>
+          </div>
+        </div>
       </div>
 
+      <!-- and lastly proiority evidence card -->
+      <div class="grid grid-cols-2 gap-4 h-48 m-5">
+        <div class="flex items-center">
+          <label for="" class="text-lg text-gray-600"
+            >If you and any priority evidence</label
+          >
+        </div>
+        <div class="">
+          <a-form-item class="mt-4">
+            <a-form-item name="dragger" no-style>
+              <a-upload-dragger
+                v-model:fileList="formState.dragger"
+                name="files"
+                action="/upload.do"
+              >
+                <p class="ant-upload-drag-icon">
+                  <InboxOutlined />
+                </p>
+                <p class="ant-upload-text">
+                  Click or drag your priority cirtificate
+                </p>
+                <p class="ant-upload-hint">
+                  Support for a single or bulk upload.
+                </p>
+              </a-upload-dragger>
+            </a-form-item>
+          </a-form-item>
+        </div>
+      </div>
     </div>
   </a-form>
 </template>
+
 <script setup>
 import { reactive } from "vue";
 const formItemLayout = {
