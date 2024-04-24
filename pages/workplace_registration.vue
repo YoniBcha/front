479f45ffe2f752065fcc80a55aaa7a18225568d5
Yo <yonansmillion29@example.com>
2024-04-25 02:36:16 +0300

    workplace form the edit and delet part is made

--- pages/workplace_registration.vue
+++ pages/workplace_registration.vue
@@ -78,6 +78,63 @@
           </div>
         </div>
       </template>
+      <template v-if="column.dataIndex === 'age'">
+        <div class="editable-cell">
+          <div
+            v-if="editableData[record.key]"
+            class="editable-cell-input-wrapper"
+          >
+            <a-input
+              v-model:value="editableData[record.key].age"
+              @pressEnter="save(record.key)"
+            />
+            <check-outlined
+              class="editable-cell-icon-check"
+              @click="save(record.key)"
+            />
+          </div>
+          <div v-else class="editable-cell-text-wrapper">
+            {{ text || " " }}
+            <edit-outlined
+              class="editable-cell-icon"
+              @click="edit(record.key)"
+            />
+          </div>
+        </div>
+      </template>
+      <template v-if="column.dataIndex === 'address'">
+        <div class="editable-cell">
+          <div
+            v-if="editableData[record.key]"
+            class="editable-cell-input-wrapper"
+          >
+            <a-input
+              v-model:value="editableData[record.key].address"
+              @pressEnter="save(record.key)"
+            />
+            <check-outlined
+              class="editable-cell-icon-check"
+              @click="save(record.key)"
+            />
+          </div>
+          <div v-else class="editable-cell-text-wrapper">
+            {{ text || " " }}
+            <edit-outlined
+              class="editable-cell-icon"
+              @click="edit(record.key)"
+            />
+          </div>
+        </div>
+      </template>
+      <template v-else-if="column.dataIndex === 'operation'">
+        <a-popconfirm
+          v-if="dataSource.length"
+          title="Sure to delete?"
+          @confirm="onDelete(record.key)"
+        >
+          <a>Delete</a>
+        </a-popconfirm>
+      </template>
       <span
         v-if="state.searchText && state.searchedColumn === column.dataIndex"
       >
@@ -101,6 +158,7 @@
           <template v-else>{{ fragment }}</template>
         </template>
       </span>
+      
     </template>
   </a-table>
 </template>
@@ -178,6 +236,11 @@ const columns = [
       }
     },
   },
+  {
+    title: "operation",
+    dataIndex: "operation",
+    key: "operation",
+  },
 ];
 const handleSearch = (selectedKeys, confirm, dataIndex) => {
   confirm();
@@ -193,6 +256,7 @@ const handleReset = (clearFilters) => {
 
 const count = computed(() => dataSource.value.length + 1);
 const editableData = reactive({});
+
 const edit = (key) => {
   editableData[key] = cloneDeep(
     dataSource.value.filter((item) => key === item.key)[0]
@@ -200,7 +264,7 @@ const edit = (key) => {
 };
 const save = (key) => {
   Object.assign(
-    dataSource.value.filter((item) => key === item.key)[0],
+    dataSource.value.find((item) => key === item.key),
     editableData[key]
   );
   delete editableData[key];
