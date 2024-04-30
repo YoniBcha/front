0509d7b58be937986e8cf5a3a4180df343fb40b0
M <yonasmillion29@gmail.com>
2024-04-30 19:16:18 +0300

    home section inside the landing page selecton is done know left is the css part

--- layouts/default.vue
+++ layouts/default.vue
@@ -19,21 +19,35 @@
 
     <!-- home section -->
     <div
-      class="fixed top-[104px] right-16 left-16 h-[500px] overflow-y-scroll custom-scrollbar"
+      class="grid grid-cols-12 fixed top-[115px] right-16 left-16 h-[600px] overflow-y-scroll custom-scrollbar"
     >
-      <div class="h-[1000px] pt-10">djlkafsjdklfhalskdjfalskh</div>
-      <div class=""></div>
+      <div class="col-span-6 flex flex-col justify-center h-[550px]">
+        <div class="text-4xl font-bold pb-5">
+          <p class="mb-2">Ldeta Subcity Enterprise</p>
+          <p>Management System</p>
+        </div>
+        <div class="text-2xl pb-3">for you and for us</div>
+        <div class="text-sm text-gray-500">
+          Our enterprise management system will <br />facilitate the work speed
+          and the customer <br />
+          and employee satisfaction.
+        </div>
+      </div>
+
+      <div class="col-span-6 flex items-center justify-center w-[600px]">
+        <img src="~assets/illus/enterprise.png" alt="" />
+      </div>
     </div>
   </div>
 </template>
 
 <style>
-  /* Hide scrollbar for Chrome, Safari, and Opera */
-  .custom-scrollbar::-webkit-scrollbar {
-    display: none;
-  }
-  /* Hide scrollbar for Firefox */
-  .custom-scrollbar {
-    scrollbar-width: none;
-  }
+/* Hide scrollbar for Chrome, Safari, and Opera */
+.custom-scrollbar::-webkit-scrollbar {
+  display: none;
+}
+/* Hide scrollbar for Firefox */
+.custom-scrollbar {
+  scrollbar-width: none;
+}
 </style>
