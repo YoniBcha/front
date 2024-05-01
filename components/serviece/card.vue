c88dbda0e2d9f6c8057f8e9e854bdaea01690dd8
M <yonasmillion29@gmail.com>
2024-05-01 03:44:11 +0300

    swiper card is made

---
+++ components/serviece/card.vue
@@ -0,0 +1,106 @@
+<template>
+  <div class="shopping-cart">
+    <ul class="cart-items w-[177px] h-40 relative">
+      <div class="absolute top-2 left-2 z-10">
+        <button><slot name="discount-percentage"></slot></button>
+      </div>
+      <!-- heart and eye icon -->
+      <div class="absolute top-2 right-2 flex flex-col text-2xl z-10">
+        <i class="bx bx-heart rounded-full"></i>
+        <i class="bx bx-show rounded-full mt-6"></i>
+      </div>
+
+      <div class="w-[80%] mx-auto mt-2">
+        <slot name="image"></slot>
+      </div>
+    </ul>
+
+    <div class="product-info h-32">
+      <h3 class="product-name font-semibold">
+        <slot name="product-name"></slot>
+      </h3>
+      <p class="product-price">
+        <slot name="product-price"></slot>
+        <span><slot name="privious-price"></slot></span>
+      </p>
+      <p class="text-[#ffad33]">
+        <i class="bx bx-star"></i><i class="bx bx-star"></i
+        ><i class="bx bx-star"></i><i class="bx bx-star"></i
+        ><i class="bx bx-star text-black"></i
+        ><span class="text-gray-600 ml-3">(88)</span>
+      </p>
+    </div>
+    <div class="card-content">
+      <button class="add-button">Add to chart</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CardView",
+};
+</script>
+
+<style scoped>
+.shopping-cart {
+  position: relative;
+  border-radius: 6px;
+  width: 180px;
+  box-shadow: 3px 5px 10px rgba(0, 0, 0, 0.3);
+  overflow: hidden;
+  transition: all 0.5s ease;
+  margin: 10px 20px;
+}
+.cart-items button {
+  background-color: #db4444;
+  color: white;
+  width: 60px;
+  margin: 0 0.1rem;
+  padding: 4px;
+  border-radius: 5px;
+}
+.cart-items {
+  /* padding: 10px; */
+  background: #db9b2460;
+  display: flex;
+  flex-direction: column;
+  list-style: none;
+  padding-bottom: 20px;
+}
+.product-info {
+  padding: 10px;
+  flex: 1;
+}
+.product-name {
+  margin: 0;
+}
+.product-price {
+  margin: 5px 0;
+}
+.product-info .product-price span {
+  text-decoration: line-through;
+  color: rgba(0, 0, 0, 0.568);
+  margin-left: 10px;
+}
+.card-content {
+  position: absolute;
+  background-color: rgba(0, 0, 0, 0.85);
+  color: white;
+  width: 100%;
+  height: 50px;
+  transform: translateY(5%);
+  text-align: center;
+  padding-top: 9px;
+  overflow: hidden;
+  transition: all 0.5s ease;
+}
+.shopping-cart:hover .card-content {
+  transform: translateY(-100%);
+}
+.shopping-cart:hover {
+  transform: scale(1.023);
+  z-index: 3;
+  cursor: pointer;
+}
+</style>
---
+++ components/swiper/grid.vue
@@ -0,0 +1,67 @@
+<template>
+  <swiper
+    :slidesPerView="5"
+    :grid="{
+      rows: 3,
+      gap: 100,
+    }"
+    :spaceBetween="0"
+    :pagination="{
+      clickable: true,
+    }"
+    :modules="modules"
+    class="mySwiper"
+  >
+    <swiper-slide v-for="grid_data in grid_datas" :key="grid_data.id">
+      <serviece-card>
+        <template v-slot:discount-percentage>
+          -{{ grid_data.discount_rate }}%
+        </template>
+        <template v-slot:image>
+          <img
+            :src="grid_data.image_url"
+            alt="Product Image"
+            class="w-[80%] !h-36 object-contain my-auto mx-auto relative mt-2"
+          />
+        </template>
+        <template v-slot:product-name>{{ grid_data.product_name }}</template>
+        <template v-slot:product-price>
+          ${{ grid_data.product_price }}
+        </template>
+        <template v-slot:previous-price>
+          <span>${{ grid_data.product_price }}</span>
+        </template>
+      </serviece-card>
+    </swiper-slide>
+  </swiper>
+</template>
+<script>
+// Import Swiper Vue.js components
+import { Swiper, SwiperSlide } from "swiper/vue";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/grid";
+import "swiper/css/pagination";
+
+import "@/style/style.css";
+
+// import required modules
+import { Grid, Pagination } from "swiper/modules";
+import { grid_datas } from "@/global/data/grid";
+
+export default {
+  name: "GridView",
+  components: {
+    Swiper,
+    SwiperSlide,
+    CardView,
+  },
+  data() {
+    return {
+      grid_datas: grid_datas,
+      modules: [Grid, Pagination],
+    };
+  },
+};
+</script>
