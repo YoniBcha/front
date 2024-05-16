9b467852a326700135bba47ea22eb0182896c490
tiNa5113 <nathnaelmengistu13@gmail.com>
2024-05-17 02:00:21 +0300

    changed the style of the profile name and image

--- layouts/adminDashboard.vue
+++ layouts/adminDashboard.vue
@@ -219,13 +219,13 @@
           </div>
         <div class="flex relative gap-3 items-center h-full pr-5">
           <div class="flex flex-col justify-center h-full">
-            <div class="text-xl">Yonas</div>
+            <div class="text-sm">Yonas</div>
             <div class="text-sm text-gray-400">Admin, enterprise manager</div>
           </div>
           <div class="">
             <img
               @click="toggleDropdown"
-              class="object-cover w-12 h-12 rounded-full ring ring-gray-300 dark:ring-gray-600"
+              class="object-cover w-10 h-10 rounded-full ring ring-gray-300 dark:ring-gray-600"
               src="https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=4&w=880&h=880&q=100"
               alt=""
             />
