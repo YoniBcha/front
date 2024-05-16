70a926a41ca1ee3f16bfa019e34440d34e98672c
tiNa5113 <nathnaelmengistu13@gmail.com>
2024-05-17 01:56:06 +0300

    added a form on the contact us page

--- pages/landingPage/contact-us.vue
+++ pages/landingPage/contact-us.vue
@@ -1,9 +1,20 @@
 <template>
-    <div class="">Contact Us Page</div>
+  <!-- <div class="text-blue-600 text-3xl text-center font-bold">CONTACT US</div> -->
+  <!-- component -->
+  <section class="text-white-600 body-font relative">
+    <div class="absolute inset-0 bg-white-300 content-center">
+      <iframe
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d31524.483087815785!2d38.71513031272523!3d9.012535660722982!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x164b867dd850c4e3%3A0x2ac81e393f995660!2sLideta%2C%20Addis%20Ababa!5e0!3m2!1sen!2set!4v1715729531881!5m2!1sen!2set"
+        width="100%"
+        height="100%"
+        style="border: 0"
+        allowfullscreen="true"
+        loading="lazy"
+        referrerpolicy="no-referrer-when-downgrade"
+      ></iframe>
+    </div>
+    
+  </section>
+  <!--FOOTER-->
+ 
 </template>
-
-<script setup>
-definePageMeta({
-  layout: "admin-dashboard",
-});
-</script>
