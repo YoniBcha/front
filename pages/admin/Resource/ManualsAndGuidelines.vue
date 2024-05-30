7ea6f2938fac362412fa99c3f2fda61b304aca38
tiNa5113 <nathnaelmengistu13@gmail.com>
2024-05-30 07:10:11 +0300

    home nav

--- pages/admin/Resource/ManualsAndGuidelines.vue
+++ pages/admin/Resource/ManualsAndGuidelines.vue
@@ -36,15 +36,6 @@
             {{ file.description }}
           </p>
         </div>
-        <div class="p-6 pt-0">
-          <a
-            :href="file.path"
-            target="_blank"
-            class="select-none rounded-lg bg-blue-500 py-2 px-4 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-grey-500/20 transition-all hover:shadow-lg hover:shadow-grey-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
-          >
-            View File
-          </a>
-        </div>
       </div>
     </div>
   </div>
